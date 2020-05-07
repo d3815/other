6cc88cebdafa7a821499a7cbf3341794faea2666
Denis Zolotarev <zolotarev@tgn-mb-0575.corp.game-insight.com>
2020-05-07 15:58:35 +0300

    prints were deleted

--- check_and_move_png.py
+++ check_and_move_png.py
@@ -12,18 +12,20 @@ path_to_png_dir = '/ipad-hd'
 
 for d, dirs, files in os.walk(os.getcwd() + path_to_png_dir):
     for one_file in files:
+
         if one_file.endswith('.png'):
             path = os.path.join(d, one_file)
-            print(path)
             work_img = Image.open(path)
             (width, height) = work_img.size
+
             if width < 256:
                 if height < 256:
                     res = '/result'
                     c = d
                     new_path = d + res
-                    print(new_path)
                     if not os.path.exists(new_path):
                         os.mkdir(new_path)
+                        
                     print(f'Файл {path} перемещен')
                     shutil.move(path, new_path)
+
